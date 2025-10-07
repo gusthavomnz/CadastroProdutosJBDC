39e013bf6bffa1088873ce1a72a4e2428a36f42d
Gusthavo <gusthavogp123@gmail.com>
2025-10-07 15:51:52 -0300

    Update Conexao.java

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/coursejbdc";
+    private static final String URL = "";
     private static final String USER = "";
     private static final String PASSWORD = "";
 
